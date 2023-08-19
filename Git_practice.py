b69b3942be00137fa1d7ed0d5780b296d6bc736c
Manuel-1122 <antwiattaemmanuel@gmail.com>
2023-08-19 07:25:32 -0500

    Commit python file

---
+++ Git_practice.py
@@ -0,0 +1,5 @@
+def compute_multiplication():
+    first_number = float(input("Type your first number: ", ))
+    second_number = float(input("Type your second number: ", ))
+    return first_number * second_number
+
